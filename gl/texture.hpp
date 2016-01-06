2ab6cc7334594e38ba386e2bebf93421d7c6d124
Alexey Gerasev <alexey.gerasev@gmail.com>
2016-01-07 00:30:51 +0600

    load textures partly

--- gl/texture.hpp
+++ gl/texture.hpp
@@ -79,8 +79,6 @@ private:
 	GLuint _id = 0;
 	int _bounds[3] = {0, 0, 0};
 	InternalFormat _in_format = RGB8;
-	Format _format = RGB;
-	Type _type = UBYTE;
 	int _dim = 2;
 	
 	static GLuint target(int dim) {
@@ -135,8 +133,6 @@ public:
 		setInterpolation(intp);
 		
 		_in_format = ifmt;
-		_format = fmt;
-		_type = type;
 		
 		if(dim == 1) {
 			glTexImage1D(GL_TEXTURE_1D, 0, (GLuint)ifmt, bounds[0], 0, (GLuint)fmt, (GLuint)type, data);
@@ -144,6 +140,23 @@ public:
 			glTexImage2D(GL_TEXTURE_2D, 0, (GLuint)ifmt, bounds[0], bounds[1], 0, (GLuint)fmt, (GLuint)type, data);
 		} else if(dim == 3) {
 			glTexImage3D(GL_TEXTURE_3D, 0, (GLuint)ifmt, bounds[0], bounds[1], bounds[2], 0, (GLuint)fmt, (GLuint)type, data);
+		}
+		
+		error = glGetError();
+		if(error != GL_NO_ERROR)
+			throw ErrorException(error);
+	}
+	
+	void loadSubData(const void *data, const int offset[], const int sub_size[], Format fmt, Type type) throw(ErrorException) {
+		GLenum error;
+		bind();
+		
+		if(_dim == 1) {
+			glTexSubImage1D(GL_TEXTURE_1D, 0, offset[0], sub_size[0], (GLuint)fmt, (GLuint)type, data);
+		} else if(_dim == 2) {
+			glTexSubImage2D(GL_TEXTURE_2D, 0, offset[0], offset[1], sub_size[0], sub_size[1], (GLuint)fmt, (GLuint)type, data);
+		} else if(_dim == 3) {
+			glTexSubImage3D(GL_TEXTURE_3D, 0, offset[0], offset[1], offset[2], sub_size[0], sub_size[1], sub_size[2], (GLuint)fmt, (GLuint)type, data);
 		}
 		
 		error = glGetError();
@@ -168,12 +181,6 @@ public:
 	}
 	InternalFormat internal_format() const {
 		return _in_format;
-	}
-	Type type() const {
-		return _type;
-	}
-	Format format() const {
-		return _format;
 	}
 };
 }

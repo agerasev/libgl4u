1841249aa91bbb2439f625c2d9d4796620f7642d
Alexey Gerasev <alexey.gerasev@gmail.com>
2016-05-21 12:24:41 +0600

    fb viewport fix

--- sources/framebuffer.cpp
+++ sources/framebuffer.cpp
@@ -10,33 +10,11 @@ FrameBuffer::~FrameBuffer() {
 }
 
 void FrameBuffer::init(Texture::InternalFormat ifmt, int width, int height) throw(Exception) {
-	_width = width;
-	_height = height;
-	
-	bind();
-	
 	int s[2] = {width, height};
-	_tex.init(2, s, ifmt);
-	
-	if(GLEW_VERSION_3_2)
-		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, _tex.id(), 0);
-	else if(GLEW_ARB_framebuffer_object)
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _tex.id(), 0);
-	else
-		throw Exception("No framebuffer support : (GLEW_VERSION_3_2 || GLEW_ARB_framebuffer_object) == 0");
-	
-	GLenum bufs[1] = {GL_COLOR_ATTACHMENT0};
-	glDrawBuffers(1, bufs);
-	
-	if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-		throw Exception("FrameBuffer create error");
-	
-	unbind();
+	init(2, ifmt, s);
 }
 
 void FrameBuffer::init(int dim, Texture::InternalFormat ifmt, const int size[]) throw(Exception) {
-	bind();
-	
 	if(dim == 1) {
 		_width = size[0];
 		_height = 1;
@@ -44,6 +22,8 @@ void FrameBuffer::init(int dim, Texture::InternalFormat ifmt, const int size[]) 
 		_width = size[0];
 		_height = size[1];
 	}
+	
+	bind();
 	
 	_tex.init(dim, size, ifmt);
 	
